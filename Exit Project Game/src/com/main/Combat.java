ce76cde3cf58dc052f5005246ecf00079b890231
jarod Sandburg <jtsandburg@gmail.com>
2019-10-16 19:56:27 -0500

    worked on: -Level 1 map -movement in levels

--- Exit Project Game/src/com/main/Combat.java
+++ Exit Project Game/src/com/main/Combat.java
@@ -1,6 +1,8 @@
 package com.main;
 
 import com.main.Entitys.Player;
+import com.main.Items.Magic.Magic;
+import com.main.Items.Swords.Swords;
 import com.main.Tutorial;
 
 public class Combat {
@@ -11,11 +13,9 @@ public class Combat {
         Helpers.print(lines);
     }
     protected void araPrint(String... lines) { Helpers.araPrint(lines);}
-
+    protected Helpers h = new Helpers();
 
     public void start(Player player, Entity enemy){
-
-        Helpers h = new Helpers();
 
         if(tutorialLevel == true){
             araPrint("Thea's are the basic steps of combat for idiots that have never plaid games before.");
@@ -27,21 +27,33 @@ public class Combat {
             araPrint("What are you expecting a deeper explanation you will literally just attack the target with your sword.");
             araPrint("The second command for when you are in combat is MAGIC.");
             araPrint("When you use this command you will aces your magic list and have to pick the magic you want to use.");
-            araPrint("The last command for when you are in combat is ITEM.");
-            araPrint("When you use this command you will go into your inventory and see all the items that are useful while in combat.");
+
         }
 
-        switch (h.menu("attack", "magic", "item")){
+        switch (h.menu("attack", "magic")){
             case "attack":
+                attack();
 
                 break;
 
             case "magic":
-
-                break;
-            case "item":
+                magic();
 
                 break;
         }
+
+
     }
+
+    private void attack(){
+        Swords s = new Swords();
+        print("You attack your enemy dealing" + s.damage + "damage");
+
+    }
+
+    private void magic(){
+        Magic m = new Magic();
+
+    }
+
 }
